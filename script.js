f151fb4799a25e1328f9542ecbe28fb7c806b9ac
Simon Baumhauer <simon.baumhauer@hotmail.ch>
2022-01-23 12:18:10 +0400

    questioncard design 1.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let questions = [{
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -7,7 +7,7 @@ let questions = [{
         "right_answer": 3
     },
     {
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -15,7 +15,7 @@ let questions = [{
         "right_answer": 3
     },
     {
-        "question": "Wer hat HTML erfunden",
+        "question": "Wer hat HTML erfunden?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -23,3 +23,29 @@ let questions = [{
         "right_answer": 3
     },
 ];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('pageNumber').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('questiontext').innerHTML = question['question']
+    document.getElementById('answer_1').innerHTML = question['answer_1']
+    document.getElementById('answer_2').innerHTML = question['answer_2']
+    document.getElementById('answer_3').innerHTML = question['answer_3']
+    document.getElementById('answer_4').innerHTML = question['answer_4']
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    console.log('Selected answer is', selection)
+    let selectedQuestionNumber = selection.slice(-1);
+    console.log('selectedQuestionNumber is', selectedQuestionNumber);
+    console.log('Selected answer is', question['right_answer']);
+}
